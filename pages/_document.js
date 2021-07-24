82f66271408a69bc5f1eebcbc85ddefaf12ea804
Nasubikun <j08012333439@gmail.com>
2021-07-25 02:25:27 +0900

    '*'

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+      <title>みんなの絵（文字）日記</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
